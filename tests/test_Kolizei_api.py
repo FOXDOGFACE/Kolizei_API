035762e88d01541a8c6ee5760d80ff14beb815a6
Foxdogface <66514567+Foxdogface@users.noreply.github.com>
2021-07-01 23:31:57 +0200

    Delete tests directory

--- tests/test_Kolizei_api.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import os
-from dotenv import load_dotenv
-import kolizei_api
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
-
