876a29494196d8025fd21aa93070b55fa8857d0c
Foxdogface <k.kasyanenko@gmail.com>
2021-07-01 19:18:37 +0200

    .

--- tests/test_Kolizei_api.py
+++ tests/test_Kolizei_api.py
@@ -1,4 +1,9 @@
-import os
 import pytest
+import os
 from dotenv import load_dotenv
+import kolizei_api
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
